560c8434857d6cb2c0db2c0e47c6cf2a121980dd
miguelmorais7 <42868106+miguelmorais7@users.noreply.github.com>
2021-02-02 13:58:17 -0300

    Add Proof-of-Work

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const SHA256 = require('crypto-js/sha256');
-
 class Block{
     constructor(index, timestamp, data, previousHash = ''){
         this.index = index;
@@ -7,16 +6,28 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log('Block mined: ' + this.hash);
     }
 }
+
 
 class Blockchain{
     constructor(){
         this.chain = [this.createGenisisBlock()];
+        this.difficulty = 4;
     }
 
     createGenisisBlock(){
@@ -29,7 +40,8 @@ class Blockchain{
 
     addBlock(newBlock){
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        //newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
     }
 
@@ -50,11 +62,19 @@ class Blockchain{
 }
 
 let savjeeCoin = new Blockchain();
-savjeeCoin.addBlock(new Block(1, '10/10/2020', { amount: 4 }));
-savjeeCoin.addBlock(new Block(2, '05/11/2020', { amount: 10 }));
 
+console.log('Mining block 1...')
+savjeeCoin.addBlock(new Block(1, '10/10/2020', { amount: 4 }));
+
+console.log('Mining block 2...')
+savjeeCoin.addBlock(new Block(2, '05/11/2020', { amount: 8 }));
+
+
+
+/*
 console.log('Is blockchain valid? ' + savjeeCoin.isChainValid());
 savjeeCoin.chain[1].data = { amount: 100 };
 savjeeCoin.chain[1].hash = savjeeCoin.chain[1].calculateHash();
 console.log('Is blockchain valid? ' + savjeeCoin.isChainValid());
 //console.log(JSON.stringify(savjeeCoin, null, 4));
+*/
